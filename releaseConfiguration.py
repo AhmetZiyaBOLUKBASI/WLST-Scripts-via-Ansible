23286813fdae3e2dcfb01b4ac18e29f4a95f8d25
AhmetZiyaBOLUKBASI <60754022+AhmetZiyaBOLUKBASI@users.noreply.github.com>
2025-04-02 01:58:51 +0300

    Initial Commit.

---
+++ releaseConfiguration.py
@@ -0,0 +1,8 @@
+connect("<your username>", "<your password>", "t3s://<your admin consol URL>:<your admin console https port>")
+cd('edit:/')
+cancelEdit('y')
+disconnect()
+print "---------------------------------------------------------------------------------"
+print " It Could Be Deploy New Package on the Server <your servername> !"
+print "---------------------------------------------------------------------------------"
+exit()
